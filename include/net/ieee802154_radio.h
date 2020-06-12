69d91bdb0032445c3cc202e30df5c8bced59bdfc
Jun Li <jun.r.li@intel.com>
2020-06-12 09:11:48 -0700

    api: ieee802154: define vendor specific config
    Add vendor specific config type and data structure
    
    Signed-off-by: Jun Li <jun.r.li@intel.com>

--- include/net/ieee802154_radio.h
+++ include/net/ieee802154_radio.h
@@ -122,7 +122,10 @@ enum ieee802154_config_type {
 	/** Specifies new radio event handler. Specifying NULL as a handler
 	 *  will disable radio events notification.
 	 */
-	IEEE802154_CONFIG_EVENT_HANDLER
+	IEEE802154_CONFIG_EVENT_HANDLER,
+
+	/** Vendor specific configuration */
+	IEEE802154_CONFIG_VENDOR
 };
 
 /** IEEE802.15.4 driver configuration data. */
@@ -150,6 +153,12 @@ struct ieee802154_config {
 
 		/** ``IEEE802154_CONFIG_EVENT_HANDLER`` */
 		ieee802154_event_cb_t event_handler;
+
+		/** ``IEEE802154_CONFIG_VENDOR`` */
+		struct {
+			const uint8_t *cfg_data;
+			uint16_t cfg_size;
+		} vendor;
 	};
 };
 
